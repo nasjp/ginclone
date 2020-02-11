104eaf5bb07dbed3c8cd5ab70ddab191d5d6b0cd
nas <nas.silver.bullet@gmail.com>
2020-02-12 01:19:43 +0900

    :+1: Refactor a little

--- ginclone.go
+++ ginclone.go
@@ -14,31 +14,35 @@ func New() *Engine {
 	return &Engine{router: make(map[string]http.HandlerFunc)}
 }
 
-func (e *Engine) addRoute(method string, pattern string, handler http.HandlerFunc) {
+func (e *Engine) addRoute(method string, pattern string, f http.HandlerFunc) {
 	key := method + "-" + pattern
 	log.Printf("Route %4s - %s", method, pattern)
-	e.router[key] = handler
+	e.router[key] = f
 }
 
-func (e *Engine) GET(pattern string, handler http.HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (e *Engine) GET(pattern string, f http.HandlerFunc) {
+	e.addRoute("GET", pattern, f)
 }
 
-func (e *Engine) POST(pattern string, handler http.HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (e *Engine) POST(pattern string, f http.HandlerFunc) {
+	e.addRoute("POST", pattern, f)
 }
 
 func (e *Engine) Run(addr string) (err error) {
+	addrMsg := addr
+	if len(addrMsg) > 0 && addrMsg[0] == ':' {
+		addrMsg = "localhost" + addrMsg
+	}
+	log.Printf("=> %s", addrMsg)
 	return http.ListenAndServe(addr, e)
 }
 
-// ServeHTTP(ResponseWriter, *Request)
-func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	handler, ok := e.router[key]
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	f, ok := e.router[key]
 	if !ok {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
 		return
 	}
-	handler(w, req)
+	f(w, r)
 }
